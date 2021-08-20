8f4db5d943d1b9868f9507a15945a3e561d7008c
David J. Barnes <davidjonbarnes@yahoo.co.uk>
2021-08-20 11:47:16 +0100

    stopServer syntax

--- stopService.sh
+++ stopService.sh
@@ -1,4 +1,4 @@
   
 #!/bin/bash
 service=monitors.service
-systemctl -q is-active $service && sudo systemctl stop $service || exit 1
+systemctl -q is-active $service && sudo systemctl stop $service || exit 0
